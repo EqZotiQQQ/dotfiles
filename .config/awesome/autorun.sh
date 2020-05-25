f66af8f55b8bd49072fa30c0a51a7ff4e32d7f84
Mikhail Fedyakov <fedyakovmikael@gmail.com>
2020-05-25 23:24:02 +0300

    lang layouts

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -1,10 +1,6 @@
 run-unique compton
 feh --bg-scale ~/Pictures/Wallpaper/mountains_lake_tops_top_view_119133_3840x2160.jpg
 
+export LC_ALL=C
 
-
-
-
-kilall -q polybar
-while pgrep -u $UID -x polybar >dev/null; do sleep 1; done
-polybar -c ~/.config/polybar/config.ini main &
+setxkbmap -layout us,ru, -option 'grp:toggle'
