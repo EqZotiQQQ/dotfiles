f526bad2c788c49414776138a534775a6e052b87
Mikhail Fedyakov <fedyakovmikael@gmail.com>
2020-07-04 21:40:07 +0300

    cosy

---
+++ .config/awesome/cosy/init.lua
@@ -0,0 +1,14 @@
+---------------------------------------------------------------------------
+--- Cosy
+-- Custom definitions
+--
+-- @module cosy
+---------------------------------------------------------------------------
+
+return {
+    util     = require("cosy.util"),
+    audio    = require("cosy.audio"),
+    sysinfo  = require("cosy.sysinfo"),
+    layout   = require("cosy.layout"),
+    widget   = require("cosy.widget"),
+}
