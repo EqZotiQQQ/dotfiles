229a7eed55ac21779d06ca8f4d967798508fcb73
Mikhail Fedyakov <fedyakovmikael@gmail.com>
2020-07-05 13:17:05 +0300

    removed themes

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -1,5 +1,5 @@
-feh --bg-scale ~/Pictures/Wallpaper/mountains_lake_tops_top_view_119133_3840x2160.jpg
-
+#feh --bg-scale ~/Pictures/Wallpaper/mountains_lake_tops_top_view_119133_3840x2160.jpg
+feh --bg-scale ~/Pictures/Wallpaper/artwork-digital-art-anime-girls-video-games-wallpaper-preview.jpg
 run-unique skypeforlinux
 
 export LC_ALL=C
