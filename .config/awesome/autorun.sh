119311cfb2e122da87cb04006723509ba6cc792a
Mikhail Fedyakov <mf_52@mail.ru>
2020-07-05 22:05:28 +0300

    rc.lua

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -1,5 +1,4 @@
-#feh --bg-scale ~/Pictures/Wallpaper/mountains_lake_tops_top_view_119133_3840x2160.jpg
-feh --bg-scale ~/Pictures/Wallpaper/artwork-digital-art-anime-girls-video-games-wallpaper-preview.jpg
+feh --bg-scale $HOME/Pictures/artwork-digital-art-anime-girls-video-games-wallpaper-preview.jpg
 run-unique skypeforlinux
 
 export LC_ALL=C
