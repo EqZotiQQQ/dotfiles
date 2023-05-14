f0662a0af59b3e81b1bc4759dc22432768b9e550
Mikhail Fedyakov <fedyakovmikael@gmail.com>
2023-05-14 12:39:39 +0300

    t

--- home/.config/awesome/configs/theme.lua
+++ home/.config/awesome/configs/theme.lua
@@ -1,7 +1,7 @@
 local home = os.getenv("HOME")
 
 local wallpapers = {
-    blue_colored_cosy_willage = home.."/Pictures/black.jpg"
+    blue_colored_cosy_willage = home.."/Pictures/1.jpg"
 }
 
 return {
