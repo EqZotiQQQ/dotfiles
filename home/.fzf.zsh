bd27e30156c3a7aa24517eaefc5e56522778d479
EqZotiQQQ <mf_52@mail.ru>
2026-08-30 12:59:37 +0300

    t

--- home/.fzf.zsh
+++ home/.fzf.zsh
@@ -1,3 +1,15 @@
 #!/bin/zsh
 
-(( $+commands[fzf] )) && source <(fzf --zsh)
+# (( $+commands[fzf] )) && source <(fzf --zsh)
+
+if (( $+commands[fzf] )); then
+  if fzf --zsh >/dev/null 2>&1; then
+    source <(fzf --zsh)
+  else
+    # fzf < 0.48 has no --zsh flag; fall back to the shipped shell scripts
+    for f in /usr/share/doc/fzf/examples/{key-bindings,completion}.zsh /usr/share/fzf/{key-bindings,completion}.zsh; do
+      [[ -r "$f" ]] && source "$f"
+    done
+  fi
+fi
+
